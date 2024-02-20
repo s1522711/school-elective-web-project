71f1ca898af6ab5fd3adcdaf5a08c6254e702a85
s1522711 <s1522711@outlook.com>
2024-02-21 01:21:54 +0200

    added nuke and fixed file formatting

--- js/nuke.js
+++ js/nuke.js
@@ -1,13 +1,13 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const disableCloseButton = async () => {
-    document.getElementById("nukeCloseButton").disabled=true;
-    for (let i = 5.00; i > 0; i-=0.01) {
-        document.getElementById("nukeCloseButton").innerHTML="Nuking in " + i.toFixed(2) + " seconds...";
+    document.getElementById("nukeCloseButton").disabled = true;
+    for (let i = 5.00; i > 0; i -= 0.01) {
+        document.getElementById("nukeCloseButton").innerHTML = "Nuking in " + i.toFixed(2) + " seconds...";
         await delay(10);
     }
-    document.getElementById("nukeCloseButton").disabled=false;
-    document.getElementById("nukeCloseButton").innerHTML="Nuke";
+    document.getElementById("nukeCloseButton").disabled = false;
+    document.getElementById("nukeCloseButton").innerHTML = "Nuke";
 }
 
 const playExplosion = async () => {
@@ -17,7 +17,7 @@ const playExplosion = async () => {
     nukeModal.hide();
     document.body.style.display = "initial";
     document.body.style.display = "none";
-    await delay(3000);
+    await delay(4000);
     document.body.style.display = "initial"; // Show the body again
 }
 
